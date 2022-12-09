1cb81c903f44fbc52162e5b1e6dcb3881d09418f
kelsie kidd <kelsiekidd93@gmail.com>
2022-12-09 10:15:48 -0600

    Initial file structure

---
+++ server/utils/auth.js
@@ -0,0 +1,28 @@
+const jwt = require('jsonwebtoken');
+
+const secret = 'wittlebittysecret';
+const expiration = '3h';
+module.exports = {
+    authmiddleware: function ({req}) {
+        let token = reqquery.token || req.headers.authorization;
+
+        if (req.headers.authorization) { 
+            token = token.split(' ').pop().trim();
+        }
+        if (!token) {
+            return req
+        }
+        try {
+            const { data } = jwt.verify(token, secret, { maxAge: expiration });
+            req.user = data;
+        } catch {
+            console.log('Invalid token');
+            return req
+        }
+        return req
+    },
+    signToken: function ({ email, _id }) {
+        const payload = { email, _id };
+        return jwt.sign({ data: payload }, secret, { expiresIn: expiration });
+    },
+};
